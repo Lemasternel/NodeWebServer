7f85460ff6103c41bb3e601b4a6049ab47796586
Leonel Liesenberg <liesenberg.leonel@gmail.com>
2017-08-30 20:34:54 -0300

    43. Rendering Templates with Data

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Andrew',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome! message.'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(1)
+  });
 });
 
 // /bad - send back json with errorMessage
@@ -26,6 +29,7 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(4000, () => {
-  console.log('Server is up on port 3000');
+var porta = 3000;
+app.listen(porta, () => {
+  console.log('Server is up on port ', porta);
 });
